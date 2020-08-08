7392eed41fb6fef521319f5286013d3d930ab02b
c00kamunga <tsothalanti2@yahoo.com>
2020-08-07 17:47:33 -0700

    added endpoint for find a post with a specified ID

--- posts-Router.js
+++ posts-Router.js
@@ -1,2 +1,34 @@
 const db = require('./data/db')
 const router = express.Router()
+
+
+//GET/api/posts-returns an array of all the post objects container in the database.
+router.get('/api/posts', async(req,res) => {
+    try {
+        const posts = await db.find()
+        res.status(200).json(posts)
+    } catch (error){
+        res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved' })
+    }
+})
+
+//GET Returns the post object with the specified ID.
+
+router.get('/api/post/:id', (req, res) => {
+    db.findByID(req.params.id)
+    .then(post => {
+        if (post.length === 0) {
+            return res.status(404)
+            .json({ message:'The post with the specified ID does not exist' })
+        } else {
+            res.status(200).json(post)
+        }
+    })
+    .catch(error => {
+        res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved' })
+    })
+})
